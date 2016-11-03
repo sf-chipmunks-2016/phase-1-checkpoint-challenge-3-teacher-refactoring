6123c898f9454fb860b486d9b46e1ca946c34cf8
April Tang <april.tang09@gmail.com>
2016-11-03 10:07:43 -0700

    Delete previous code

--- senior_teacher.rb
+++ senior_teacher.rb
@@ -7,25 +7,11 @@ class SeniorTeacher < DBC
 
   attr_reader :salary, :performance_rating, :target_raise
 
-  # attr_reader :age, :salary, :phase, :performance_rating, :target_raise
-  # attr_accessor :name
-
   def initialize(options={})
     super
     @phase = 3
-    # @age = options.fetch(:age, 0)
-    # @name = options.fetch(:name, "")
     @target_raise = 1000
   end
-
-  # def offer_high_five
-  #   "High five!"
-  # end
-
-  # def set_phase(num)
-  #   @phase = num
-  #   "Cool, I've always wanted to teach phase #{num}!"
-  # end
 
   def teach_stuff
     response = ""
@@ -34,15 +20,6 @@ class SeniorTeacher < DBC
     response += "... You're welcome. *saunters away*"
     response
   end
-
-  # def salary=(new_salary)
-  #   puts "This better be good!"
-  #   @salary = new_salary
-  # end
-
-  # def receive_raise(raise)
-  #   @salary += raise
-  # end
 
   def set_performance_rating(rating)
     response = ""
