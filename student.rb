97c2479fb5e888b5210cf124dfbc4d4cf0a178ca
meganeding1git config --global user.email eding.megan@gmail.comgit config --global core.editor subl <eding.megan@gmail.com>
2016-11-03 09:35:51 -0700

    refactor student responses

--- student.rb
+++ student.rb
@@ -8,10 +8,8 @@ class Student < Person
   end
 
   def set_phase(num)
-    response = ""
     if num == @phase
-      response += "I'm doing phase #{@phase} again because "
-      response += "I put my learning first. I'm gonna rock it!"
+      response = "I'm doing phase #{@phase} again because I put my learning first. I'm gonna rock it!"
     else
       response = "Oooh, phase #{num}. I hope I'm ready!"
     end
@@ -20,9 +18,6 @@ class Student < Person
   end
 
   def learn_stuff
-    response = ""
-    response += "WHOA! I've never thought of it quite like that before. "
-    response += "Now I feel like a genius!"
-    response
+    "WHOA! I've never thought of it quite like that before. Now I feel like a genius!"
   end
 end
