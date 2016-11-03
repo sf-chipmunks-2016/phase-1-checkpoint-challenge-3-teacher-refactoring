1e5df439ed6bc8f477143d016f04f11cac31ff76
April Tang <april.tang09@gmail.com>
2016-11-03 10:07:30 -0700

    Delete previous code

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -7,25 +7,11 @@ class ApprenticeTeacher < DBC
 
   attr_reader :salary, :target_raise
 
-  # attr_reader :age, :salary, :phase, :target_raise
-  # attr_accessor :name
-
   def initialize(options={})
     super
-    # @age = options.fetch(:age, 0)
-    # @name = options.fetch(:name, "")
     @target_raise = 800
     @phase = 3
   end
-
-  # def offer_high_five
-  #   "High five!"
-  # end
-
-  # def set_phase(num)
-  #   @phase = num
-  #   "Cool, I've always wanted to teach phase #{num}!"
-  # end
 
   def teach_stuff
     response = ""
@@ -34,15 +20,6 @@ class ApprenticeTeacher < DBC
     response += "... You're welcome."
     response
   end
-
-  # def salary=(new_salary)
-  #   puts "This better be good!"
-  #   @salary = new_salary
-  # end
-
-  # def receive_raise(raise)
-  #   @salary += raise
-  # end
 
   def set_performance_rating(rating)
     response = ""
