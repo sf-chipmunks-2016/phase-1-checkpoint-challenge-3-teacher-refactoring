66d0aeef14f2f348e4e8d1dff44206032fd00eec
JessCodes <jessicafellison@gmail.com>
2016-11-03 10:03:36 -0700

    Add super class of people_in_school, add parent class of staff to the two teachers, add greetable module because all high five. Add constants to staff class for rating threshhold and teach_stuff responses

---
+++ greetable.rb
@@ -0,0 +1,5 @@
+module Greetable
+  def offer_high_five
+    "High five!"
+  end
+end
---
+++ staff.rb
@@ -0,0 +1,40 @@
+require_relative 'people_in_school'
+
+class Staff < PeopleInSchool
+  attr_reader :salary, :target_raise
+
+  def set_phase(num)
+    @phase = num
+    "Cool, I've always wanted to teach phase #{num}!"
+  end
+
+  def salary=(new_salary)
+    puts "This better be good!"
+    @salary = new_salary
+  end
+
+  def receive_raise(raise)
+    @salary += raise
+  end
+  
+  def teach_stuff
+    response = ""
+    response += self.class::RESPONSE2
+    response += self.class::RESPONSE3
+    response += self.class::RESPONSE4
+    response
+  end
+
+    def set_performance_rating(rating)
+    response = ""
+    if rating > self.class::RATING_THRESHOLD
+      response = "Yay, I'm a great employee!"
+      receive_raise(@target_raise)
+    else
+      response += "Oh, well -- thanks to this actionable, specific, and kind "
+      response += "feedback, I'll do better next time."
+    end
+    response
+  end
+
+end
